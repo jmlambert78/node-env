27a441afb0b09edba196a08006d08e825809f3e2
jmlambert78 <dummy@gmail.com>
2016-02-16 10:52:57 +0000

    Updated page server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var http = require("http"),
     keyprocessor = require("./lib/keyprocessor"),
     sysinfo = require('./lib/sysinfo'),
     port = process.env.PORT || 8080;
-
+    
 http.createServer(function(request, response) {
 
     var uri = url.parse(request.url).pathname,
