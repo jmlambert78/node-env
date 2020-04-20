9783d299277259ac1b86d75eb421c98242ac7ae5
jmlambert78 <jmlambert78@gmail.com>
2020-04-20 15:10:17 +0200

    param tag

--- launch-pipeline.sh
+++ launch-pipeline.sh
@@ -1,8 +1,9 @@
+tagVersion=${1:-0.1}
 tkn pipeline start \
   --param=pathToContext="." \
   --param=pathToYamlFile="deploy.yaml" \
   --param=imageUrl="jmlambert78/nodeenv" \
-  --param=imageTag="0.1" \
+  --param=imageTag="$tagVersion" \
   --serviceaccount="service-account" \
   application-pipeline
 
